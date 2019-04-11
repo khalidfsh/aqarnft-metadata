edb478634b87187c1c36800b34cdcb0d0aa68973
khalidfsh <dev.khalid@me.com>
2019-04-12 02:59:46 +0300

    Fix data

--- src/database.js
+++ src/database.js
@@ -21,60 +21,60 @@ module.exports = {
     "type": "villa"
   },
   "3": {
-    "name": "Pool Villa",
-    "sq_foot": "800",
-    "color": "white",
-    "rating": 9,
-    "type": "villa"
-  },
-  "4": {
-    "name": "Family House, Islamic design",
-    "sq_foot": "1500",
+    "name": "House",
+    "sq_foot": "1200",
     "color": "tan",
     "rating": 10,
     "type": "house"
   },
-  "5": {
+  "4": {
     "name": "Midagies Modern Villa",
     "sq_foot": "1200",
     "color": "tan",
     "rating": 10,
     "type": "villa"
   },
-  "6": {
-    "name": "Modern Apartment",
-    "sq_foot": "500",
-    "color": "tan",
-    "rating": 7,
-    "type": "apartnent"
-  },
-  "7": {
+  "5": {
     "name": "Villa",
     "sq_foot": "1000",
     "color": "tan",
     "rating": 9,
     "type": "villa"
   },
+  "6": {
+    "name": "House",
+    "sq_foot": "1200",
+    "color": "tan",
+    "rating": 10,
+    "type": "house"
+  },
+  "7": {
+    "name": "Family House, Islamic design",
+    "sq_foot": "1500",
+    "color": "tan",
+    "rating": 10,
+    "type": "house"
+  },
   "8": {
+    "name": "Pool Villa",
+    "sq_foot": "800",
+    "color": "white",
+    "rating": 9,
+    "type": "villa"
+  },
+  "9": {
     "name": "Swiss House",
     "sq_foot": "1500",
     "color": "tan",
     "rating": 10,
     "type": "house"
   },
-  "9": {
-    "name": "House",
-    "sq_foot": "1200",
-    "color": "tan",
-    "rating": 10,
-    "type": "house"
-  },
   "10": {
-    "name": "House",
-    "sq_foot": "1200",
+    "name": "Modern Apartment",
+    "sq_foot": "500",
     "color": "tan",
-    "rating": 10,
-    "type": "house"
+    "rating": 7,
+    "type": "apartnent"
   }
   
 }
